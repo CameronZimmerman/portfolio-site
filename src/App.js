a1e530d6b2e6b7e8617f9595f002110be2d2ff81
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-05-10 11:57:45 -0700

    initialization, add home page with basic interactive iframe template

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./Home/Home";
+
+export default function App() {
+  return (
+    <Router>
+      <h1 className="name-heading">
+        🌳CAMERON ZIMMERMAN🌳
+      </h1>
+      <div className="main-content">
+        <nav className="center">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/bio">Bio</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/experience">Experience</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route> */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ src/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="center-column">
+
+      <section className="interactive-invitation-text center">
+        <p>Feeling tired? Take a walk with me through my <a href="#interactive-iframe">interactive Portfolio</a> instead.</p>
+      </section>
+
+      <section className="main-text center">
+        <p>Thank you taking the time to visit my portfolio. Maybe you're a fellow developer, recruiter, teacher, or hiring manager; regardless, it is my intention to open up a dialogue and generate discussion with you. I'm inclined to not only show you what I have done, but learn and iterate from my accomplishments based off of feedback.</p>
+      </section>
+
+      <section className="interactive-portfolio-container center" id="interactive-iframe">
+        <iframe src="./interactive/interactive.html" title="Interactive Portfolio">
+
+        </iframe>
+      </section>
+    </div>
+  )
+}
